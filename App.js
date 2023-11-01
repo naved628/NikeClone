43997199f5ad0349a599ff245410f8bb03a6061b
CA-NAVEDK <naved.khan@crossasyst.com>
2023-11-01 20:02:12 +0530

    installed stripe payment and also added apiSlice file for payment

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
-import Navigation from "./src/navigation";
 import { Provider } from "react-redux";
+import {StripeProvider} from '@stripe/stripe-react-native';
+
 import { store } from "./src/store";
+import Navigation from "./src/navigation";
 
 export default function App() {
+  
+  const STRIPEKEY = 'pk_test_51O7LWKSIHJUOV15yd9KlZOU6gD9i2cWmBuyvHFJNnyd5b22IACaOxpUBqOG1jp5c0joTDveAWZQvX3RqRFWRxjI5007lKiIEW0';
+  
   return (
     <Provider store={store}>
+      <StripeProvider publishableKey={STRIPEKEY}>
         <Navigation />
         <StatusBar style="auto" />
+      </StripeProvider>
     </Provider>
   );
 }
